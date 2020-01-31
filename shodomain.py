fe0b2d8810a3aa599d3acf3232958194b4465661
SmoZy <58377338+SmoZy92@users.noreply.github.com>
2020-01-31 15:35:39 +0100

    Update shodomain.py

--- shodomain.py
+++ shodomain.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
-import requests, sys
+import requests, sys, json
 from bs4 import BeautifulSoup as Soup, Tag
      
 if __name__ == "__main__":
     if len (sys.argv) != 2 :
-            print "Shodan Subdomain Finder by SmoZy\n\nUsage: ./shodan.py [domain] "
+            print "Shodan Subdomain Finder by SmoZy\n\nUsage: ./shodan.py [API Key] [Domain]"
             sys.exit (1)
 
-domain = sys.argv[1]
-r =requests.get('https://beta.shodan.io/domain/' + domain)
-soup = Soup(r.text, 'html.parser')
-table = soup.find("table", { "class" : "feature-table u-full-width" })
-for row in table.findAll("tr"):
-    cells = row.findAll("td")
-    if len(cells) == 3:
-        entry = cells[0].find(text=True)
-        record_type = cells[1].find(text=True)
-        value = cells[2].find(text=True)
-        if record_type == 'CNAME' and domain in value: 
-            delim = value.split('.')
-            match = delim[-2] + '.' + delim[-1]
-            if match == domain:
-                print "{}.{}".format(entry, value)
+apikey = sys.argv[1]
+domain = sys.argv[2]
+
+r =requests.get('https://api.shodan.io/dns/domain/' + domain + '?key=' + apikey)
+data = json.loads(r.text)
+for item in data["data"]:
+    entry = item["subdomain"]  
+    record_type = item["type"]
+    value = item["value"]
+    if record_type == 'CNAME' and domain in value: 
+        delim = value.split('.')
+        match = delim[-2] + '.' + delim[-1]
+        if match == domain:
+            print "{}".format(value)
+
+    
